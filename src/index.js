0b1daae7cb942a035a6d27db3d8caf7a6df34fd3
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-01-11 17:40:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countyInfo = document.querySelector('.country-info');
 
+input.addEventListener('keyup', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(evt) {
+  evt.preventDefault();
+  const name = evt.target.value.trim();
+
+  if (!name) {
+    countyInfo.innerHTML = '';
+    return;
+  }
+  getCountry(name).then(data => (countyInfo.innerHTML = murkupCountry(data)));
+}
+
 function getCountry(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=capital,population,languages,name,flags`
@@ -16,56 +29,31 @@ function getCountry(name) {
   });
 }
 
-input.addEventListener('keyup', debounce(onInput, DEBOUNCE_DELAY));
+// function countryName(data) {
+//   data.map(
+//     () => {
 
-function onInput(evt) {
-  evt.preventDefault();
-  const name = evt.target.value.trim();
+//     }
+//   );
+// }
 
-  if (!name) {
-    countyInfo.innerHTML = '';
-    return;
-  }
-  getCountry(name).then(data => countryName(data));
-}
-
-function countryName(data) {
-  data.map(
-    ({
-      flags: { svg },
-      name: { official },
-      capital,
-      population,
-      languages,
-    }) => {
-      if (data.length === 1) {
-        countyInfo.innerHTML = murkupCountry({
-          flags: { svg },
-          name: { official },
-          capital,
-          population,
-          languages,
-        });
-      } else if (data.length < 2 || data.length < 10) {
-        console.log(data);
-      }
-    }
-  );
-}
-
-function murkupCountry({
-  flags: { svg },
-  name: { official },
-  capital,
-  population,
-  languages,
-}) {
-  return `<ul>
+function murkupCountry(arr) {
+  return arr
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+        capital,
+        population,
+        languages,
+      }) => `<ul>
     <li><h2>
         <img src="${svg}" alt="flag" width="50" height="30" />
         ${official}</h2></li>
     <li><h3>Capital: ${capital}</h3></li>
     <li><h3>Population: ${population}</h3></li>
     <li><h3>Languages: ${Object.values(languages)}</h3></li>
-    </ul>`;
+    </ul>`
+    )
+    .join('');
 }
